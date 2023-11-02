3971e414d5db31325b51c0611d53db2bba806d09
tangchuan <tangchuanjiaoshou@gmail.com>
2023-11-02 21:50:55 +0800

    add handleSumbit

--- src/component/Form.js
+++ src/component/Form.js
@@ -1,8 +1,14 @@
 import React from "react";
 
 function Form(props) {
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        alert("Hello World!");
+    }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2 className="label-wrapper">
         <label htmlFor="new-todo-input" className="label__lg">
           What needs to be done?
