9f1df4b975c98566ba4115314d61a999dfb665f8
tangchuan <tangchuanjiaoshou@gmail.com>
2023-11-02 10:57:19 +0800

    start TodoMatic

--- src/index.js
+++ src/index.js
@@ -7,9 +7,12 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App subject="tangchuan" />
   </React.StrictMode>
 );
+
+// ReactDOM.render(<App subject="Clarice" />, document.getElementById("root"));
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
