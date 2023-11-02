214541fa81f7d0ac187b2d04a8ef26b6ebc4b54b
tangchuan <tangchuanjiaoshou@gmail.com>
2023-11-02 22:12:58 +0800

    Updating state

--- src/component/Form.js
+++ src/component/Form.js
@@ -6,29 +6,37 @@ function Form(props) {
 
     function handleSubmit(e) {
         e.preventDefault();
-        alert("Say hello!");
-    }
+        props.addTask(name);
+        setName("");
+      }
+      
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <h2 className="label-wrapper">
-        <label htmlFor="new-todo-input" className="label__lg">
-          What needs to be done?
-        </label>
-      </h2>
-      <input
-        type="text"
-        id="new-todo-input"
-        className="input input__lg"
-        name="text"
-        autoComplete="off"
-        value={name}
-      />
-      <button type="submit" className="btn btn__primary btn__lg">
-        Add
-      </button>
-    </form>
-  );
+    function handleChange(e) {
+        setName(e.target.value);
+      }
+      
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <h2 className="label-wrapper">
+                <label htmlFor="new-todo-input" className="label__lg">
+                    What needs to be done?
+                </label>
+            </h2>
+            <input
+                type="text"
+                id="new-todo-input"
+                className="input input__lg"
+                name="text"
+                autoComplete="off"
+                value={name}
+                onChange={handleChange}
+            />
+            <button type="submit" className="btn btn__primary btn__lg">
+                Add
+            </button>
+        </form>
+    );
 }
 
 export default Form;
